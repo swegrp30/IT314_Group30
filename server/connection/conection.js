53a78f4dfecdcb22826b6d39d2a1bb15982aa2ef
natvar55 <202101402@daiict.ac.in>
2023-09-30 15:19:13 +0530

    Backend added

---
+++ server/connection/conection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+mongoose.set('strictQuery', false);
+
+// mongodb://localhost:27017/
+mongoose.connect("mongodb+srv://backend2:backend2@cluster0.qxwolth.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    // useFindAndModify:false
+    // useCreateIndex:true
+}).then(()=>{
+    console.log("Connected To MONGO-DB");
+}).catch((err)=>{
+    console.log("ERROR FROM connect.js")
+    console.log(err);
+})
+
+
