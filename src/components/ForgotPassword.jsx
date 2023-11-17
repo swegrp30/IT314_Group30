68f6f22e69922f9badddb01047f50a86c0936fe4
LvarunL <varunvyas312@gmail.com>
2023-11-18 00:27:28 +0530

    Updated the forgotPassword to include OTP confirmation.

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,31 +1,51 @@
 import React from 'react';
 import '../style/Profile.css';
-
+import { useState } from 'react';
 function Forgotpassword() {
+    const [gotOTP,setGotOTP] = useState(false);
+    const [gotEmail,setGotEmail] = useState(false);
     return (
-       
         <div className="forgotPW">
             <div className='r_cp'>
                 FORGOT PASSWORD
             </div>
             <div className='rc'>
                 <div className='right_name1'>
-                    <div className='rmain1'>
-                        <div className='r1'>Email ID</div>
-                        <input className='input_box' />
-                    </div>
-                   
-                    <div className='rmain1'>
-                        <div className='r1'>New Password</div>
-                        <input className='input_box' />
-                    </div>
-                    <div className='rmain1'>
-                        <div className='r1'>Confirm Password</div>
-                        <input className='input_box' />
-                    </div>
-                    <button className='r_profile_btn2'>
+                    { !gotEmail && <>
+                        <div className='rmain1'>
+                            <div className='r1'>Email ID</div>
+                            <input className='input_box' />
+                        </div>
+                        <button className='r_profile_btn2' onClick={()=>setGotEmail(true)}>
+                            Get OTP
+                        </button>
+                    </>}
+                    
+                    { gotEmail && !gotOTP && <>
+                        <div className='rmain1'>
+                            <div className='r1'>Enter OTP</div>
+                            <input className='input_box' />
+                        </div>
+                        <button className='r_profile_btn2' onClick={()=>setGotOTP(true)}>
+                            Submit
+                        </button>
+                    </>}
+
+
+                    { gotOTP && <>
+                        <div className='rmain1'>
+                            <div className='r1'>New Password</div>
+                            <input className='input_box' />
+                        </div>
+                        <div className='rmain1'>
+                            <div className='r1'>Confirm Password</div>
+                            <input className='input_box' />
+                        </div>
+                        <button className='r_profile_btn2'>
                         Confirm
-                    </button>
+                        </button>
+                    </>}
+                    
                 </div>
                 
             </div>
