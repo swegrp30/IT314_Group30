5378d5d863d0bd8b99652ca9e8712a9f6e9ab752
Shubham Patel <120598942+Bot-Binary@users.noreply.github.com>
2023-11-17 17:15:00 +0000

    **

--- server/general_functions/addReplytoComment.js
+++ server/general_functions/addReplytoComment.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const rply = (async(req,res)=>{
 
+    const user = req.userData.username;
+    const comment = req.body.comment;
+    const commentID = req.body.commentID;
+
     
 
 })
