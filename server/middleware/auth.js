e0326daa84730bab05ee636409821426cdf79b2d
Bhavya418 <shahb3703@gmail.com>
2023-11-18 22:07:45 +0530

    updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
     try {
-        const token = req.headers.cookie;
+        const token = req.header("auth-token");
         console.log(token);
         const data = jwt.verify(token, process.env.JWT_KEY);
         req.userData = data
