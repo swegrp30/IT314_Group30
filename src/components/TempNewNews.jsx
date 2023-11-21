6547d90cd2446ce7c460c0c11f654b99411532b5
GOR-OM <202101484@daiict.ac.in>
2023-11-21 13:00:44 +0530

    Create New Newspage (Better version)

--- src/components/TempNewNews.jsx
+++ src/components/TempNewNews.jsx
@@ -1,122 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../style/TempNews.css'
+import DrawerExample from './DrawerExample'
+import genralNewsData from '../Json/generalNewsData.json'
 
 const TempNewNews = () => {
-  return (
-    <div>
-        <header class="showcase">
-        <div class="container">
-            <div class="text-content">
-                <p class="sports-category">sports</p>
-                <h1>some sports article</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-                    temporibus nesciunt in aut facilis optio eum, et nisi obcaecati
-                    accusamus reprehenderit iste facere nobis, qui eaque totam,
-                    laboriosam sequi animi?
-                </p>
-                <a href="./html/article.html">Read More</a>
-            </div>
+    const [newsData, setNewsData] = useState([]);
+
+    useEffect(() => {
+        // Fetch data when the component mounts
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            // const response = await fetch(genralNewsData);
+            // const data = await response.json();
+            setNewsData(genralNewsData.stories.slice(0, 7)); // Use the first 8 stories
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const createArticle = (articleData) => (
+        <a key={articleData.id} href={articleData.url} className="card">
+            {articleData.img && <img src={articleData.img} alt="" />}
+            <article>
+                {articleData.category && (
+                    <p className={`${articleData.category.toLowerCase()}-category`}>
+                        {articleData.category}
+                    </p>
+                )}
+                <h3>{articleData.title}</h3>
+                {/* <p>{articleData.description}</p> */}
+            </article>
+        </a>
+    );
+
+    return (
+        <div>
+            <DrawerExample />
+            <section>
+                <div className="container">
+                    <div className="articles">
+                        {newsData.map((article) => createArticle(article))}
+                    </div>
+                </div>
+            </section>
+
         </div>
-    </header>
-
-    <section>
-        <div class="container">
-            <h1 class="editor-h1">Editor Picks</h1>
-            <div class="articles">
-                <a href="./html/article.html" class="card">
-                    <img src="./images/articles/ent1.jpg" alt="" />
-                    <article>
-                        <p class="entertainment-category">Entertainment</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <article>
-                        <p class="sports-category">Sports</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <img src="./images/articles/tech1.jpg" alt="" />
-                    <article>
-                        <p class="technology-category">Technology</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <article>
-                        <p class="sports-category">Sports</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                    <img src="./images/articles/sports1.jpg" alt="" />
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <img src="./images/articles/tech2.jpg" alt="" />
-                    <article>
-                        <p class="technology-category">Technology</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <article>
-                        <p class="sports-category">Sports</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                </a>
-
-                <a href="./html/article.html" class="card">
-                    <article>
-                        <p class="entertainment-category">Entertainment</p>
-                        <h1>Lorem ipsum dolor sit amet.</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-                            nisi neque eum libero maiores voluptatem repudiandae quos
-                            perspiciatis, reiciendis dolor!
-                        </p>
-                    </article>
-                    <img src="./images/articles/ent2.jpg" alt="" />
-                </a>
-            </div>
-        </div>
-    </section>
-    </div>
-  )
+    )
 }
 
 export default TempNewNews
