64309d987c423512f783847c7cf45cc3533038f5
GOR-OM <202101484@daiict.ac.in>
2023-11-25 02:23:41 +0530

    make card responsive

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,39 +1,37 @@
+
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import '../style/stock.css'
+import { Link } from "react-router-dom";
+import '../style/stock.css';
+
 const Stock = (prop) => {
-    
-    
   return (
-            <div className="col-4">
-             <div className="card l-bg-blue-dark">
-              <div className="card-statistic-3 p-4">
-                <div className="card-icon card-icon-large">
-                  <i className="fas fa-users"></i>
-                </div>
-                <Link to={`/share/${prop.name}`}>
-                <div className="mb-4">
-                  <h5 className="card-title mb-0">{prop.name}</h5>
-                </div>
-                <div className="row align-items-center mb-2 d-flex">
-                  <div className="col-8">
-                    <h2 className="d-flex align-items-center mb-0">$ {prop.lastClose}</h2>
-                  </div>
-                  <div className="col-4 text-right">
-                    <span>
-                      {prop.lastChange}% <i className="fa fa-arrow-up"></i>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                    <button>Redirect</button>
-                </div>
-                    </Link>
+    <div className="col-lg-4 col-md-6 col-sm-12">
+      <div className="card l-bg-blue-dark">
+        <div className="card-statistic-3 p-4">
+          <div className="card-icon card-icon-large">
+            <i className="fas fa-users"></i>
+          </div>
+          <Link to={`/share/${prop.name}`}>
+            <div className="mb-4">
+              <h5 className="card-title mb-0">{prop.name}</h5>
+            </div>
+            <div className="row align-items-center mb-2 d-flex">
+              <div className="col-8">
+                <h2 className="d-flex align-items-center mb-0">$ {prop.lastClose}</h2>
+              </div>
+              <div className="col-4 text-right">
+                <span>
+                  {prop.lastChange}% <i className="fa fa-arrow-up"></i>
+                </span>
               </div>
             </div>
+            <div>
+              <button>Redirect</button>
             </div>
-          
-        
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
