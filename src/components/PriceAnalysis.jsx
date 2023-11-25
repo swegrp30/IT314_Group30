e71bbba61dccce1c688d606d921e84dae4988929
natvar55 <202101402@daiict.ac.in>
2023-11-25 22:06:43 +0530

    wishlist buttons added

--- src/components/PriceAnalysis.jsx
+++ src/components/PriceAnalysis.jsx
@@ -1,39 +1,72 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Stock from './Stock';
-
+import { toast } from 'react-toastify';
 const PriceAnalysis = () => {
-    const [share,setShare] = useState([]);
+  const [share, setShare] = useState([]);
+  const token = localStorage.getItem('authToken');
+  
+  const headers = {
+    "Content-Type": "application/json",
+    "auth-token": token,
+  };
+  const handleaddfav = async (e) => {
+    // console.log(prop.name);
+    try {
+      const res = await axios.post("http://localhost:7000/add-fav", {
+        company: e,
+      }, { headers });
+      const data = res.status;
+      if (data === 200) {
+        toast.success("Added to Favourites");
+      }
+    } catch (err) {
+      if (err.response) {
+        // ✅ log status code here
+        console.log(err.response.status);
+        console.log(err.message);
+        console.log(err.response.headers); // 👉️ {... response headers here}
+        console.log(err.response.data); // 👉️ {... response data here}
+      }
+    }
+  };
 
-    useEffect(() => {    
-        const handleClick =async()=>{
-        const res = await axios.get('http://localhost:7000/getdata');
-        console.log(res.data)
-        setShare(res.data);
-        
+  useEffect(() => {
+    const handleClick = async () => {
+      const res = await axios.get('http://localhost:7000/getdata');
+      console.log(res.data)
+      setShare(res.data);
+
     }
     handleClick();
-    
+
   }, [share]);
-  
+
   return (
 
     <div>
-        <div className='row m-5'>
-            
-        {share.map((item,index)=>
-        
-               <Stock name={share[index].Name}
-               ticker = {share[index].Ticker}
-                       lastClose ={share[index].LastClose}
-        lastChange = {share[index].LastChange}
-        >
+      <div className='row m-5'>
+
+        {share.map((item, index) =>
+          <>
+            <Stock
+              page='priceanalysis'
+              name={share[index].Name}
+              ticker={share[index].Ticker}
+              lastClose={share[index].LastClose}
+              lastChange={share[index].LastChange}
+            >
             </Stock>
-            
-            )}
-        
-        
-        </div>  
+            <div className="col-12 text-right">
+              <button className='btn btn-primary' onClick={handleaddfav}>
+                add-fav
+              </button>
+            </div>
+          </>
+        )}
+
+
+      </div>
     </div>
   )
 }
