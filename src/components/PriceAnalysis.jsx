6d45d9a877e0c16e1b96c35c2091ec39c4af329f
GOR-OM <202101484@daiict.ac.in>
2023-11-25 23:15:17 +0530

    add loader and reduce time for loding using save data

--- src/components/PriceAnalysis.jsx
+++ src/components/PriceAnalysis.jsx
@@ -1,73 +1,176 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import Stock from './Stock';
+// import { toast } from 'react-toastify';
+// import Loader from './Loader';
+// import ErrorPage from './ErrorPage';
+// const PriceAnalysis = () => {
+//   const [share, setShare] = useState([]);
+//   const token = localStorage.getItem('authToken');
+//   const [loading, setLoading] = useState(true);
+  
+//   const headers = {
+//     "Content-Type": "application/json",
+//     "auth-token": token,
+//   };
+//   const handleaddfav = async (e) => {
+//     // console.log(prop.name);
+//     try {
+//       const res = await axios.post("http://localhost:7000/add-fav", {
+//         company: e,
+//       }, { headers });
+//       const data = res.status;
+//       if (data === 200) {
+//         toast.success("Added to Favourites");
+//       }
+//     } catch (err) {
+//       if (err.response) {
+//         // ✅ log status code here
+//         console.log(err.response.status);
+//         console.log(err.message);
+//         console.log(err.response.headers); // 👉️ {... response headers here}
+//         console.log(err.response.data); // 👉️ {... response data here}
+//       }
+//     }
+//   };
+  
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const res = await axios.get('http://localhost:7000/getdata');
+//         setShare(res.data);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//         <ErrorPage />
+//       } finally {
+//         setLoading(false); // Set loading to false when data is fetched (or if there's an error)
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+
+//     <div>
+//     {loading ? ( // Show Loader while data is being fetched
+//       <Loader />
+//     ) : (
+//       <div className='row m-5'>
+
+//         {share.map((item, index) =>
+//           <>
+//             <Stock
+//               name={share[index].Name}
+//               ticker={share[index].Ticker}
+//               lastClose={share[index].LastClose}
+//               lastChange={share[index].LastChange}
+//             >
+//             </Stock>
+//             <div className="col-12 text-right">
+//               <button className='btn btn-primary' onClick={()=>handleaddfav(item.Name)}>
+//                 add-fav
+//               </button>
+//             </div>
+//           </>
+//         )}
+
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default PriceAnalysis
+
+
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Stock from './Stock';
+import Loader from './Loader';
+import ErrorPage from './ErrorPage';
 import { toast } from 'react-toastify';
+
 const PriceAnalysis = () => {
   const [share, setShare] = useState([]);
+  const [loading, setLoading] = useState(true);
   const token = localStorage.getItem('authToken');
-  
+
   const headers = {
-    "Content-Type": "application/json",
-    "auth-token": token,
+    'Content-Type': 'application/json',
+    'auth-token': token,
   };
+
   const handleaddfav = async (e) => {
-    // console.log(prop.name);
     try {
-      const res = await axios.post("http://localhost:7000/add-fav", {
-        company: e,
-      }, { headers });
+      const res = await axios.post('http://localhost:7000/add-fav', { company: e }, { headers });
       const data = res.status;
       if (data === 200) {
-        toast.success("Added to Favourites");
+        toast.success('Added to Favourites');
       }
     } catch (err) {
       if (err.response) {
-        // ✅ log status code here
         console.log(err.response.status);
         console.log(err.message);
-        console.log(err.response.headers); // 👉️ {... response headers here}
-        console.log(err.response.data); // 👉️ {... response data here}
+        console.log(err.response.headers);
+        console.log(err.response.data);
       }
     }
   };
-  
+
   useEffect(() => {
-    const handleClick = async () => {
-      const res = await axios.get('http://localhost:7000/getdata');
-      console.log(res.data)
-      setShare(res.data);
+    const fetchData = async () => {
+      try {
+        // Check if data is available in session storage
+        const storedData = sessionStorage.getItem('priceAnalysisData');
+        if (storedData) {
+          setShare(JSON.parse(storedData));
+        } else {
+          // Fetch data from the server if not available in session storage
+          const res = await axios.get('http://localhost:7000/getdata');
+          const data = res.data;
+          setShare(data);
 
-    }
-    handleClick();
+          // Save data to session storage
+          sessionStorage.setItem('priceAnalysisData', JSON.stringify(data));
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle error state if needed
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  }, [share]);
+    fetchData();
+  }, []); // Run only once when the component mounts
 
   return (
-
     <div>
-      <div className='row m-5'>
-
-        {share.map((item, index) =>
-          <>
-            <Stock
-              name={share[index].Name}
-              ticker={share[index].Ticker}
-              lastClose={share[index].LastClose}
-              lastChange={share[index].LastChange}
-            >
-            </Stock>
-            <div className="col-12 text-right">
-              <button className='btn btn-primary' onClick={()=>handleaddfav(item.Name)}>
-                add-fav
-              </button>
-            </div>
-          </>
-        )}
-
-
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className='row m-5'>
+          {share.map((item, index) => (
+            <React.Fragment key={index}>
+              <Stock
+                name={share[index].Name}
+                ticker={share[index].Ticker}
+                lastClose={share[index].LastClose}
+                lastChange={share[index].LastChange}
+              />
+              <div className='col-12 text-right'>
+                <button className='btn btn-primary' onClick={() => handleaddfav(item.Name)}>
+                  add-fav
+                </button>
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PriceAnalysis
+export default PriceAnalysis;
