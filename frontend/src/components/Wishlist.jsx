d2a4eebb505b8135c19cb5c84b2c66d375b70868
VasuGolakiya <vasugolakiya2344@gmail.com>
2023-11-29 17:51:20 +0530

    Removed "Add to Fav" button in wishlist page stocks

--- frontend/src/components/Wishlist.jsx
+++ frontend/src/components/Wishlist.jsx
@@ -1,10 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; 
 import ErrorPage from './ErrorPage';
 import '../style/Wishlist.css';
-import Stock from './Stock';
 import { toast } from 'react-toastify';
 import Loader from './Loader';
+
+const Indian = ["Reliance Industries", "Infosys", "HDFC Bank", "Tata Consultancy Services"];
+
+const StockWithoutAddToFav = ({ name, lastClose, lastChange }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const linkStyle = isHovered
+        ? { color: '#fff', textDecoration: 'underline' } 
+        : { color: '#fff', textDecoration: 'none' };  
+
+    const scaleStyle = isHovered ? { transform: "scale(1.1)" } : {};
+
+    return (
+        <div className="card l-bg-blue-dark mb-3" style={{ transition: "all 0.3s", ...scaleStyle }}>
+            <div className="card-statistic-3 p-4">
+                <div className="card-icon card-icon-large">
+                    <i className="fas fa-users"></i>
+                </div>
+                <Link
+                    to={`/share/${name}`}
+                    className="text-decoration-none text-white"
+                    style={linkStyle}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <div className="mb-4">
+                        <h3 className="card-title mb-0">{name}</h3>
+                    </div>
+                    <div className="row align-items-end mb-2">
+                        <div className="col-8">
+                            <h2 className="d-flex align-items-center mb-0">
+                                {Indian.includes(name) ? "₹" : "$"} {lastClose}
+                            </h2>
+                        </div>
+                        <div className="col-4 text-right">
+                            <span className={`text-success font-weight-bold`} style={{ fontSize: "20px" }}>
+                                {lastChange}%
+                            </span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
 
 const Wishlist = () => {
     const [share, setShare] = useState([]);
@@ -16,6 +70,7 @@ const Wishlist = () => {
         'Content-Type': 'application/json',
         'auth-token': token,
     };
+
     const handledelfav = async (e) => {
         try {
             const res = await axios.post("http://localhost:7000/del-fav", {
@@ -35,6 +90,7 @@ const Wishlist = () => {
             }
         }
     };
+
     useEffect(() => {
         const getWishlistData = async () => {
             try {
@@ -61,7 +117,7 @@ const Wishlist = () => {
 
     if (loading) {
         return <div>
-            <Loader/>
+            <Loader />
         </div>;
     }
 
@@ -77,19 +133,18 @@ const Wishlist = () => {
             <div className="row m-5">
                 {share && share.map((item, index) => (
                     item ? (
-                        <>
-                            <Stock
-                                key={index}
+                        <div key={index}>
+                            <StockWithoutAddToFav
                                 name={item.Ticker}
                                 lastClose={item.LastClose}
                                 lastChange={item.LastChange}
                             />
                             <div className="col-12 text-right">
-                                <button className='btn btn-primary' onClick={()=>handledelfav(item.Name)}>
+                                <button className='btn btn-primary' onClick={() => handledelfav(item.Name)}>
                                     remove-fav
                                 </button>
                             </div>
-                        </>
+                        </div>
                     ) : null
                 ))}
             </div>
@@ -97,4 +152,4 @@ const Wishlist = () => {
     );
 };
 
-export { Wishlist };
+export { Wishlist, StockWithoutAddToFav };
