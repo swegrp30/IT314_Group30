ccab7cb01b6ad6616f00caeebe35dc28c8389465
Darshana <202101467@daiict.ac.in>
2023-11-29 23:47:18 +0530

    testing of pages

---
+++ backend/server/test/forgotPassword.spec.js
@@ -0,0 +1,42 @@
+// let chai = require("chai");
+// let chaiHttp = require("chai-http");
+// chai.should();
+// chai.use(chaiHttp);
+// describe("Testing of Forgot Password", () => {
+//   const host = 'http://localhost:7000';
+//   const path = '/forgotPassword';
+  
+//   it("With Valid Credential", (done) => {
+//     chai
+//       .request(host)
+//       .post(path)
+//       .send({
+//         email: "tappy3002@gmail.com",
+//         otp_number:"245226"
+//       })
+//       .end(function (err, res, body) {
+        
+
+//         res.status.should.equal(500);
+//         done();
+//       });
+//   });
+
+// //   it("With Invalid Credential", (done) => {
+// //     chai
+// //       .request(host)
+// //       .post(path)
+// //       .send({
+// //         email: "1234@daiict.ac.in",
+// //         otp_number:"265226"
+// //       })
+// //       .end(function (err, res, body) {
+        
+
+// //         res.status.should.equal(288);
+// //         done();
+// //       });
+
+// //   });
+  
+// });
