c8e4353a587ee6caf8537d0acc59df5f340ae626
Darshana <202101467@daiict.ac.in>
2023-11-29 22:44:07 +0530

    sign up page

--- backend/server/test/signup.spec.js
+++ backend/server/test/signup.spec.js
@@ -31,4 +31,32 @@ describe("Testing of Sign Up", () => {
         done();
       });
   });
+
+  it("Admin With All Invalid Credentials", (done) => {
+    chai
+      .request(host)
+      .post(path)
+      .send({
+        name: "Darshana",
+        phone: "9408997272",
+        email: "202101467@daiict.ac.in",
+        password: "Tappy",
+        username: "Darshana",
+        gender:"Female",
+        city:"Rajkot",
+        state:"Gujarat",
+        country:"India",
+        pincode:"360006",
+        occupation:"Student",
+        dob:"20/07/2003"
+      })
+      .end(function (err, res, body) {
+        
+
+        res.status.should.equal(399);
+        done();
+      });
+      
+  });
+  
 });
