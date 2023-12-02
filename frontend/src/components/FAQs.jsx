b872d95eb67fafd94b13f1f7165a9d79584c57b5
GOR-OM <202101484@daiict.ac.in>
2023-12-03 02:49:25 +0530

    added FAQs page

--- frontend/src/components/FAQs.jsx
+++ frontend/src/components/FAQs.jsx
@@ -1,44 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import '../style/ContactUs.css';
+import React from 'react';
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Text, Box, Container } from "@chakra-ui/react";
 
-const ContactUs = () => {
+const FAQs = () => {
+  return (
+    <Container maxW="lg" mt={10}>
+      <Text fontSize="3xl" fontWeight="bold" textAlign="center" mb={6}>
+        Frequently Asked Questions
+      </Text>
 
-    return (
-        <div className='contactus-container '>
-            <div class="accordion accordion-flush" id="accordionFlushExample">
-                <div class="accordion-item contactus-sub-container">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Email
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body"> swegrp30@gmail.com </div>
-                    </div>
-                </div>
-                <div class="accordion-item contactus-sub-container">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Phone Number
-                        </button>
-                    </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">123-456-789</div>
-                    </div>
-                </div>
-                <div class="accordion-item contactus-sub-container">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            Address
-                        </button>
-                    </h2>
-                    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body"> DAIICT, Reliance Cross Rd, Gandhinagar, Gujarat 382007 </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+      <Accordion allowToggle>
+        {faqData.map((faq, index) => (
+          <AccordionItem key={index}>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  {faq.question}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>{faq.answer}</Text>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+
+     
+    </Container>
+  );
 };
 
-export default ContactUs;
+const faqData = [
+  {
+    question: "How accurate are stock market predictions?",
+    answer: "Stock market predictions vary in accuracy. They're based on analysis, historical data, and various models, but the market can be unpredictable.",
+  },
+  {
+    question: "What methods are used for stock market predictions?",
+    answer: "Predictions often use technical analysis (chart patterns, trends), fundamental analysis (company financials), and sometimes sentiment analysis or machine learning algorithms.",
+  },
+  // ... Add other questions and answers
+];
+
+export default FAQs;
