cedb0b0c1a8d15fd2c1574b71ff6e7c42fa674d0
VasuGolakiya <vasugolakiya2344@gmail.com>
2023-12-02 16:18:15 +0530

    Changed text area in contactus page and added some style

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -4,36 +4,36 @@ import '../style/ContactUs.css';
 const ContactUs = () => {
 
     return (
-        <div className=''>
+        <div className='contactus-container '>
             <div class="accordion accordion-flush" id="accordionFlushExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Accordion Item #1
+                            Email
                         </button>
                     </h2>
                     <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                        <div class="accordion-body"> swegrp30@gmail.com </div>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Accordion Item #2
+                            Phone Number
                         </button>
                     </h2>
                     <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                        <div class="accordion-body">123-456-789</div>
                     </div>
                 </div>
                 <div class="accordion-item">
                     <h2 class="accordion-header">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            Accordion Item #3
+                            Address
                         </button>
                     </h2>
                     <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        <div class="accordion-body"> DAIICT, Reliance Cross Rd, Gandhinagar, Gujarat 382007 </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/DrawerExample.jsx
+++ frontend/src/components/DrawerExample.jsx
@@ -37,98 +37,98 @@ function DrawerExample() {
                         <Input p={'2'} placeholder='Type here...' />
                         <VStack p={'2'}>
 
-                        <Link to='/news'> 
-                            <Button w={'full'} colorScheme='green' mt={3} mb={3} >
-                                General News
-                            </Button>
+                            <Link to='/news'>
+                                <Button w={'full'} colorScheme='green' mt={3} mb={3} >
+                                    General News
+                                </Button>
                             </Link>
 
-                            <Link to='/tesla'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Tesla
-                            </Button>
+                            <Link to='/tesla'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Tesla
+                                </Button>
                             </Link>
 
-                            <Link to='/Apple'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Apple
-                            </Button>
+                            <Link to='/Apple'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Apple
+                                </Button>
                             </Link>
-                            <Link to='/amazon'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Amazon
-                            </Button>
+                            <Link to='/amazon'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Amazon
+                                </Button>
                             </Link>
-                            <Link to='/google'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Google
-                            </Button>
+                            <Link to='/google'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Google
+                                </Button>
                             </Link>
-                            <Link to='/netflix'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Netflix
-                            </Button>
+                            <Link to='/netflix'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Netflix
+                                </Button>
                             </Link>
-                            <Link to='/cnbc'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Cnbc
-                            </Button>
+                            <Link to='/cnbc'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Cnbc
+                                </Button>
                             </Link>
-                            <Link to='/hdfc'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                HDFC
-                            </Button>
-                            </Link>
-
-
-
-                            <Link to='/reliance'> 
-                            <Button w={'full'} colorScheme='blue' >
-                               Reliance 
-                            </Button>
-                            </Link>
-                            <Link to='/reliancegolbal'> 
-                            <Button w={'full'} colorScheme='blue' >
-                            Reliance Global
-                            </Button>
-                            </Link>
-                            <Link to='/icici'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                ICICI Bank
-                            </Button>
-                            </Link>
-                            <Link to='/infosys'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Infosys
-                            </Button>
-                            </Link>
-                            <Link to='/wipro'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Wipro
-                            </Button>
-                            </Link>
-                            <Link to='/makemytrip'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Make My Trip
-                            </Button>
+                            <Link to='/hdfc'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    HDFC
+                                </Button>
                             </Link>
 
-                            <Link to='/yatra'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Yatra
-                            </Button>
+
+
+                            <Link to='/reliance'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Reliance
+                                </Button>
+                            </Link>
+                            <Link to='/reliancegolbal'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Reliance Global
+                                </Button>
+                            </Link>
+                            <Link to='/icici'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    ICICI Bank
+                                </Button>
+                            </Link>
+                            <Link to='/infosys'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Infosys
+                                </Button>
+                            </Link>
+                            <Link to='/wipro'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Wipro
+                                </Button>
+                            </Link>
+                            <Link to='/makemytrip'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Make My Trip
+                                </Button>
                             </Link>
 
-                            <Link to='/sify'> 
-                            <Button w={'full'} colorScheme='blue' >
-                                Sify Tech
-                            </Button>
+                            <Link to='/yatra'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Yatra
+                                </Button>
                             </Link>
 
-                            <Link to='/freshworks'> 
-                            <Button w={'full'} colorScheme='blue' >
-                            Freshworks
-                            </Button>
+                            <Link to='/sify'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Sify Tech
+                                </Button>
+                            </Link>
+
+                            <Link to='/freshworks'>
+                                <Button w={'full'} colorScheme='blue' >
+                                    Freshworks
+                                </Button>
                             </Link>
 
 
