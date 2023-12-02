6494c5a94eb6612f69baf7fc59d6f5200406843f
GOR-OM <202101484@daiict.ac.in>
2023-12-03 02:35:17 +0530

    add contact us page

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
+
+import React from 'react';
 import '../style/ContactUs.css';
 
 const ContactUs = () => {
-
     return (
-        <div className='contactus-container '>
-            <div ClassName="accordion accordion-flush" id="accordionFlushExample">
-                <div ClassName="accordion-item contactus-sub-container">
-                    <h2 ClassName="accordion-header">
-                        <button ClassName="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <div className='contactus-container'>
+            <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div className="accordion-item contactus-sub-container">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                             Email
                         </button>
                     </h2>
-                    <div id="flush-collapseOne" ClassName="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div ClassName="accordion-body"> swegrp30@gmail.com </div>
+                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">swegrp30@gmail.com</div>
                     </div>
                 </div>
-                <div ClassName="accordion-item contactus-sub-container">
-                    <h2 ClassName="accordion-header">
-                        <button ClassName="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <div className="accordion-item contactus-sub-container">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                             Phone Number
                         </button>
                     </h2>
-                    <div id="flush-collapseTwo" ClassName="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div ClassName="accordion-body">+91-8200090380</div>
+                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">+91-8200090380</div>
                     </div>
                 </div>
-                <div ClassName="accordion-item contactus-sub-container">
-                    <h2 ClassName="accordion-header">
-                        <button ClassName="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                <div className="accordion-item contactus-sub-container">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                             Address
                         </button>
                     </h2>
-                    <div id="flush-collapseThree" ClassName="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div ClassName="accordion-body"> DAIICT, Reliance Cross Rd, Gandhinagar, Gujarat 382007 </div>
+                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div className="accordion-body">DAIICT, Reliance Cross Rd, Gandhinagar, Gujarat 382007</div>
                     </div>
                 </div>
             </div>
