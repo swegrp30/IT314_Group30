4159371afe4a2d4adb3ca933d37c214f23bb782c
GOR-OM <202101484@daiict.ac.in>
2023-12-03 01:47:29 +0530

    add search component

--- frontend/src/components/PriceAnalysis.jsx
+++ frontend/src/components/PriceAnalysis.jsx
@@ -6,6 +6,7 @@ import Loader from './Loader';
 import { toast } from 'react-toastify';
 import { HStack } from '@chakra-ui/react';
 import "../style/search.css";
+import Search from './Search';
 const PriceAnalysis = () => {
   const [share, setShare] = useState([]);
   
@@ -41,15 +42,7 @@ const PriceAnalysis = () => {
       ) : (
         <>
         
-  <div className="row height  d-flex justify-content-center align-items-center  ">
-    <div className="col-md-6 ">
-      <div className="form ">
-        <i className="fa fa-search"></i>
-        <input type="text" className="form-control form-input" placeholder="Search anything..." />
-        <span className="left-pan"></span>
-      </div>
-    </div>
-  </div>
+        <Search />
         
         <div className='row m-2'>
           {share.map((item, index) => (
@@ -63,9 +56,9 @@ const PriceAnalysis = () => {
             </div>
           ))}
         </div>
-        </>
+        
+      </>
       )}
-      
     </div>
   );
 };
